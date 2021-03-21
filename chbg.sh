63a26ee84c61d733d3994e293c33c0e89af24124
E. Almqvist <elalmqvist@gmail.com>
2021-03-21 21:43:28 +0100

    Added more scripts

---
+++ chbg.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+BG_PATH=$1
+
+if test -f "$BG_PATH"; then
+	echo -e "\e[1mUpdating background.\e[0m"
+	nitrogen --set-zoom-fill $BG_PATH
+	echo -e "\e[1mIf you have multiple screens then you might have to set the background yourself as nitrogen thinks that your array of screen is itself only one screen.\e[0m"
+
+	echo -e "\e[1mUpdating lockscreen.\e[0m"
+	betterlockscreen -r 1920x1080 -u $BG_PATH --dimblur
+
+	echo -e "\e[1mUpdating background image.\e[0m"
+	cp $BG_PATH $HOME/.wallpaper.jpg
+	exit
+else
+	echo -e "\e[1m[Error]: No such file\e[0m"
+	exit
+fi
