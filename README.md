c9d85c610b7b9532be243ffe506811e21f5ade67
E. Almqvist <elalmqvist@gmail.com>
2021-03-21 21:40:57 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Scripts
+Useful scripts etc.
+
+## Install
+Just symlink them into `/usr/bin` or something.
---
+++ week.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+api_url="https://vecka.nu/"
+
+week=$(curl -s $api_url | pup "time text{}")
+
+echo Week: $week
