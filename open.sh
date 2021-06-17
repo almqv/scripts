012355443834ed05a76e139039bbde3786b78a46
E. Almqvist <elalmqvist@gmail.com>
2021-06-17 18:51:00 +0200

    Updated open.sh

--- open.sh
+++ open.sh
@@ -8,10 +8,19 @@ filename="${filename%.*}"
 case $extension in
 	
 	# Video media
+	webm | mpg | mp2 | mpeg | mpe | mpv | ogg | mp4 | m4p | m4v | avi | wmv | mov | qt | flv | swf | avchd )
+		$VIDPLAY $file
+		;;
 	
 	# Picture media
+	jpeg | jpg | png | gif | tiff | psd | raw )
+		$IMGVIEW $file
+		;;
 
 	# Webpages / URL
+	html | html5 )
+		$BROWSER $file
+		;;
 
 	# File
 	*)
