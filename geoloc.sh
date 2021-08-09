3e25feaf3218b3d9d94ff007939eef3bc772d7ed
E. Almqvist <elalmqvist@gmail.com>
2021-08-09 23:13:04 +0200

    Added geo location script

---
+++ geoloc.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/zsh
+
+API_URL=http://ip-api.com/batch
+IP_FILE=$1
+
+ip_lst="[$(while read ip; do; echo -n "\"$ip\", "; done < $1 | sed 's/..$//')]"
+req_data=$(curl -s $API_URL --data $ip_lst)
+
+geo_lst=$(echo $req_data | jq -c '.[] | "\(.query): \(.country) - \(.city)"' | sed 's/\"//g' | uniq | sort)
+echo $geo_lst
