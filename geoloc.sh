ad0935da4d7c784d47f04759e391885cceda6e43
E. Almqvist <elalmqvist@gmail.com>
2021-08-10 15:22:23 +0200

    Format change for geoloc

--- geoloc.sh
+++ geoloc.sh
@@ -6,5 +6,5 @@ IP_FILE=$1
 ip_lst="[$(while read ip; do; echo -n "\"$ip\", "; done < $1 | sed 's/..$//')]"
 req_data=$(curl -s $API_URL --data $ip_lst)
 
-geo_lst=$(echo $req_data | jq -c '.[] | "\(.query): \(.country) - \(.city)"' | sed 's/\"//g' | uniq | sort)
+geo_lst=$(echo $req_data | jq -c '.[] | "\(.country) - \(.city): \(.query)"' | sed 's/\"//g' | uniq | sort)
 echo $geo_lst
