d2e0ef924d5565a4d268797798914d7b489ea6f4
E. Almqvist <elalmqvist@gmail.com>
2021-08-21 00:43:57 +0200

    Refactor

--- gitm.sh
+++ gitm.sh
@@ -34,14 +34,14 @@ fi
 
 
 new_repo() {
-	sudo mkdir -p /srv/git/$name && cd /srv/git/$name && sudo git init --bare && cd - && sudo chown -R git /srv/git/$name && sudo chgrp -R git /srv/git/$name && echo "Created new repository \"$name\"."
+	sudo mkdir -p /srv/git/$name ; cd /srv/git/$name ; sudo git init --bare ; cd - ; sudo chown -R git /srv/git/$name ; sudo chgrp -R git /srv/git/$name ; echo "Created new repository \"$name\"."
 
 }
 
 del_repo() {
-	sudo rm -rf /srv/git/$name && echo "Deleted repository \"$name\"."
+	sudo rm -rf /srv/git/$name ; echo "Deleted repository \"$name\"."
 }
 
 mov_repo() {
-	sudo mv /srv/git/$name /srv/git/$name2 && echo "Renamed repository \"$name\" to \"$name2\"."
+	sudo mv /srv/git/$name /srv/git/$name2 ; echo "Renamed repository \"$name\" to \"$name2\"."
 }
